0ec0e1f6964fc8ed2086042c09864f9d98cc5da9
victo <victoriatrotter95@gmail.com>
2022-06-06 12:06:47 -0400

    Reimbursement Models

--- src/main/java/com/revature/ers/daos/Reimbursement_StatusDAO.java
+++ src/main/java/com/revature/ers/daos/Reimbursement_StatusDAO.java
@@ -1,4 +1,32 @@
 package com.revature.ers.daos;
 
-public class Reimbursement_StatusDAO {
+import com.revature.ers.models.Reimbursement_Status;
+
+import java.util.List;
+
+public class Reimbursement_StatusDAO implements CrudDAO<Reimbursement_Status> {
+    @Override
+    public void save(Reimbursement_Status obj) {
+
+    }
+
+    @Override
+    public void update(Reimbursement_Status obj) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public Reimbursement_Status getById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Reimbursement_Status> getAll() {
+        return null;
+    }
 }
