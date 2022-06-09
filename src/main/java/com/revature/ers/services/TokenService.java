24a252e67223e82179759c02580c7d599071a11e
ML <labrecquemegan@hotmail.com>
2022-06-09 15:02:54 -0400

    added password encryption, added token creations. Fixed userDAO for usernameandpassword

---
+++ src/main/java/com/revature/ers/services/TokenService.java
@@ -0,0 +1,25 @@
+package com.revature.ers.services;
+
+import com.revature.ers.dtos.responses.Principal;
+import com.revature.ers.utils.JwtConfig;
+import com.revature.ers.utils.annotations.Inject;
+
+public class TokenService {
+    @Inject
+    private JwtConfig jwtConfig;
+
+    public TokenService() {};
+
+    @Inject
+    public TokenService(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    public String generateToken(Principal subject) {
+        return "";
+    }
+
+    public Principal extractRequesterDetails(String token) {
+        return null;
+    }
+}
